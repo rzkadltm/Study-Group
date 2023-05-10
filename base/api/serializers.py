03bfdb6931b7aa50006c5cb630bbe530e63e46c7
rzkadltm <rzkadltm@gmail.com>
2023-05-10 20:57:16 +0700

    Done

--- base/api/serializers.py
+++ base/api/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework.serializers import ModelSerializer
 from base.models import Room
 
+# take all the room's field and turn it into json
+
 
 class RoomSerializer(ModelSerializer):
     class Meta:
